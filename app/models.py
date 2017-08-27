acccb50c5486f91bfe5eb0a804c362c9207bb5b2
Mateusz Bednarski <echelon133@gmail.com>
2017-08-27 19:00:34 +0200

    Added methods to the Notes model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (Column, String, Text, Bool, DateTime)
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
 
 Base = declarative_base()
@@ -13,3 +14,30 @@ class Notes(Base):
     expiration = Column(DateTime())
     content = Column(Text())
     expired = Column(Bool())
+
+    def set_expiration(self, num):
+        
+        self.HOURS = {0: (1, 'One hour'), 
+                      1: (6, 'Six hours'),
+                      2: (12, 'Twelve hours'),
+                      3: (24, 'One day'),
+                      4: (24 * 7, 'One week'), 
+                      5: (-1, 'Never')}
+        
+        now = datetime.datetime.now()
+        additional_hours = self.HOURS[num][0]
+        expiration = now + datetime.timedelta(hours=additional_hours)
+        self.expiration = expiration
+
+    def mark_as_expired(self):
+        '''
+        Set note status to expired, so it is no longer displayed.
+        '''
+        self.expired = True
+
+    def get_expiration_as_text(self, num):
+        return self.HOURS[num][1]
+        
+    @property
+    def serialize(self):
+        return {'hash': self.hash, 'title': self.title, 'content': self.content}
