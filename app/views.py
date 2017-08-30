0e832cfca45dd0e851832871bc0a38e2d07c571d
Mateusz Bednarski <echelon133@gmail.com>
2017-08-30 16:18:22 +0200

    404 errorhandler added in views

--- app/views.py
+++ app/views.py
@@ -62,3 +62,8 @@ def get_note(hash):
             else:
                 return render_template('display.html', title=note.title,
                                        content=note.content)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
